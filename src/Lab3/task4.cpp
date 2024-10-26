5f929554368a94e20ab71a9147a85dddadf62330
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2024-10-26 12:20:27 +0100

    Update task4.cpp

--- src/Lab3/task4.cpp
+++ src/Lab3/task4.cpp
@@ -8,8 +8,12 @@ int main()
     srand(static_cast<unsigned>(time(0)));
     int sum = 0;
     for(int i = 0; i < 420; i++){
-        sum += rand()%16 + 8;
+        int k = rand()%16 + 8;
+        cout << k << ' ';
+        sum += k;
     }
+
+    cout << '\n';
 
     cout << "Guess the sum : ";
     int k;
